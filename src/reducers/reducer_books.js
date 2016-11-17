c83dd18fd8763b5cd2a901960f5729062256ae1c
ghoshabhi <abhighosh18@gmail.com>
2016-11-17 13:58:08 -0800

    Init

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'Javascript: The Good Parts' },
+    {title: 'Harry Potter and The Goblet of Fire' },
+    {title: 'Two States'},
+    {title: 'Not a Penny More, Not a Penny Less'}
+  ]
+}
